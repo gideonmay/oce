011d83a1d2702d59a73a2fedeacb502f5bc3b687
gideon may <gideon@borges.xyz>
2016-08-11 13:06:55 +0200

    Added WfObj loader tests.

---
+++ test/WfObjImport_test/WfObjImport_test.cpp
@@ -0,0 +1,46 @@
+#include <TopoDS_Shape.hxx>
+#include <RWWfObj.hxx>
+#include <gtest/gtest.h>
+
+#include "WfObjImport_test_config.h"
+
+TEST(WfObjImportTestSuite, testImportWfObj1)
+{
+    Standard_CString aFileName = (Standard_CString) wfobj_file_1;
+    TopoDS_Shape aShape;
+    Standard_Boolean result = WfObjAPI::Read(aShape,aFileName);
+    ASSERT_TRUE(result);
+    ASSERT_FALSE(aShape.IsNull());
+}
+
+TEST(WfObjImportTestSuite, testImportWfObj2)
+{
+    Standard_CString aFileName = (Standard_CString) wfobj_file_2;
+    TopoDS_Shape aShape;
+    Standard_Boolean result = WfObjAPI::Read(aShape,aFileName);
+    ASSERT_TRUE(result);
+    ASSERT_FALSE(aShape.IsNull());
+}
+
+TEST(WfObjImportTestSuite, testImportWfObj3)
+{
+    Standard_CString aFileName = (Standard_CString) wfobj_file_3;
+    TopoDS_Shape aShape;
+    Standard_Boolean result = WfObjTools::Read(aShape,aFileName,aBuilder);
+    ASSERT_TRUE(result);
+    ASSERT_FALSE(aShape.IsNull());
+}
+
+TEST(WfObjImportTestSuite, testImportWfObj4)
+{
+    Standard_CString aFileName = (Standard_CString) wfobj_file_4;
+    TopoDS_Shape aShape;
+    Standard_Boolean result = WfObjAPI::Read(aShape,aFileName);
+    ASSERT_TRUE(result);
+    ASSERT_FALSE(aShape.IsNull());
+}
+
+int main(int argc, char **argv){
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ test/WfObjImport_test/WfObjImport_test_config.h.cmake
@@ -0,0 +1,9 @@
+#ifndef WFOBJIMPORT_TEST_CONFIG_H
+#define WFOBJIMPORT_TEST_CONFIG_H
+ 
+#cmakedefine wfobj_file_1 "@wfobj_file_1@"
+#cmakedefine wfobj_file_2 "@wfobj_file_2@"
+#cmakedefine wfobj_file_3 "@wfobj_file_3@"
+#cmakedefine wfobj_file_4 "@wfobj_file_4@"
+
+#endif
